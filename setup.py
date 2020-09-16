0015944604a3485f8d0972f3c98d0d819d7baa4a
sagorbrur <sagorhem3532@gmail.com>
2020-09-16 20:06:27 +0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import codecs
+import setuptools
+
+
+setuptools.setup(
+    name="fillblank",
+    version="1.0.0",
+    author="Sagor Sarker",
+    author_email="sagorhem3532@gmail.com",
+    description="Fill the blank using transformers",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sagorbrur/fillblank",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        "transformers",
+    ],
+)
